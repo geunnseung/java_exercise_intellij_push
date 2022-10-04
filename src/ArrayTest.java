809787836fd4202e6d070b071f8cb59c4f24ce4a
seunggeun <bsg1017@naver.com>
2022-10-04 13:23:36 +0900

    1~10까지 배열만들어 출력

---
+++ src/ArrayTest.java
@@ -0,0 +1,15 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        // 10칸의 int array를 만들고
+        // 1부터 10까지 채우기
+        // 출력
+
+        int[] intNum = new int[10];
+
+        for(int i=0; i<10 ; i++){
+            intNum[i] = i+1;
+            System.out.println(intNum[i]);
+        }
+    }
+}
